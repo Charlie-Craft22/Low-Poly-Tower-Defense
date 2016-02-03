db5af0a0908ab194b4b800ef6cc1094916536c0d
Primael Quemerais <Primael Quemerais>
2016-02-03 16:00:18 +0100

    Made folders

---
+++ RTS/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float moveSpeed = 0.5f;
+    
+	void Start ()
+    {
+
+    }
+	
+	void Update ()
+    {
+        float mousePosX = Input.mousePosition.x;
+        float mousePosY = Input.mousePosition.y;
+        int scrollDistance = 5;
+        float scrollSpeed = 70;
+
+        if (mousePosX < scrollDistance)
+        {
+            transform.Translate(Vector3.right * -scrollSpeed * Time.deltaTime);
+        }
+
+        if (mousePosX >= Screen.width - scrollDistance)
+        {
+            transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime);
+        }
+
+        if (mousePosY < scrollDistance)
+        {
+            transform.Translate(transform.forward * -scrollSpeed * Time.deltaTime);
+        }
+
+        if (mousePosY >= Screen.height - scrollDistance)
+        {
+            transform.Translate(transform.forward * scrollSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ RTS/Assets/Scripts/Enemies/Enemies.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemies : MonoBehaviour
+{
+    float moveSpeed = 0.5f;
+
+	void Start ()
+    {
+	    
+	}
+	
+	void Update ()
+    {
+	    
+	}
+}
